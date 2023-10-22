aea93798ac1ce44b654932ae3e6213e2b02b015f
Shivadh <shivadh28@gmail.com>
2023-10-23 01:19:57 +0200

    King Setup

---
+++ OnionEngine/King.cpp
@@ -0,0 +1,17 @@
+#include "King.h"
+#include <iostream>
+#include <GL/freeglut.h>
+#include <glm/glm.hpp>
+
+void King::drawGeometry()
+{
+    // Implement the geometry of the pawn using OpenGL commands
+    // Customize the geometry to represent a pawn
+    glPushMatrix();
+    glScalef(0.5f, 0.5f, 1.0f); // Adjust the size as needed
+
+    // Draw the pawn as a simple cube (you can modify this)
+    glutSolidCube(1.0f);
+
+    glPopMatrix();
+}
